68a042a0c010bae9c7a6c752041d19fe469299d8
michaelerichsen <michael.erichsen@xact.dk>
2021-07-04 18:22:32 +0200

    Descendant Counter command line program added

---
+++ net.merichse.gedcom.decendantcounter/src/net/merichse/gedcom/decendantcounter/DescendantCounter.java
@@ -0,0 +1,91 @@
+package net.merichse.gedcom.decendantcounter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import org.gedcom4j.exception.GedcomParserException;
+import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.Individual;
+import org.gedcom4j.parser.GedcomParser;
+
+/**
+ * Main class for a GEDCOM decendant counter
+ * 
+ * @author michael
+ *
+ */
+public class DescendantCounter {
+
+	/**
+	 * @param iwccMap
+	 * 
+	 */
+	private static void listAncestors(Map<String, IndividualWithChildCount> sorted) {
+		for (Entry<String, IndividualWithChildCount> entry : sorted.entrySet()) {
+			IndividualWithChildCount iwcc = entry.getValue();
+			System.out.println(iwcc);
+		}
+		
+		System.out.println("Antal: " + sorted.size());
+	}
+
+	/**
+	 * @param <T>
+	 * @param args
+	 */
+	public static <T> void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Usage: DescendantCounter gedcomfilename");
+			System.exit(4);
+		}
+
+		try {
+			Gedcom gedcom = readGedcom(args[0]);
+			Map<String, IndividualWithChildCount> iwccMap = populateAncestors(gedcom);
+			TreeMap<String, IndividualWithChildCount> sorted = new TreeMap<String, IndividualWithChildCount>(iwccMap);
+			listAncestors(sorted);
+		} catch (IOException | GedcomParserException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @param gedcom
+	 * @return
+	 */
+	private static Map<String, IndividualWithChildCount> populateAncestors(Gedcom gedcom) {
+		Map<String, Individual> individuals = gedcom.getIndividuals();
+		Map<String, IndividualWithChildCount> iwccMap = new HashMap<String, IndividualWithChildCount>();
+
+		for (Entry<String, Individual> individual : individuals.entrySet()) {
+			String key = individual.getKey();
+			Individual value = individual.getValue();
+			int size = value.getDescendants().size();
+
+			if (size > 50) {
+				IndividualWithChildCount iwcc = new IndividualWithChildCount(key, value, size);
+				iwccMap.put(individual.getKey(), iwcc);
+			}
+		}
+
+		return iwccMap;
+	}
+
+	/**
+	 * @param filename
+	 * @return
+	 * @throws GedcomParserException
+	 * @throws IOException
+	 */
+	private static Gedcom readGedcom(String filename) throws IOException, GedcomParserException {
+		GedcomParser gp = new GedcomParser();
+		gp.load(filename);
+		Gedcom gedcom = gp.getGedcom();
+		return gedcom;
+	}
+
+}
---
+++ net.merichse.gedcom.decendantcounter/src/net/merichse/gedcom/decendantcounter/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Descendant counter for individuals in a GEDCOM file
+ * 
+ * @author Michael Erichsen
+ *
+ */
+package net.merichse.gedcom.decendantcounter;
