d252db67e2dffd8617fcd72791790d461f174ab8
michaelerichsen <michael@myerichsen.net>
2023-03-02 16:25:25 +0100

    Archive searcher improved

--- src/net/myerichsen/gedcom/db/SearchArchives.java
+++ src/net/myerichsen/gedcom/db/SearchArchives.java
@@ -4,17 +4,19 @@ import java.awt.Desktop;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashSet;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
- * Find all registry entries that match the phonetic name of a given individual
- * in the derby database
+ * Find all registry entries that match the phonetic name and life span of a
+ * given individual in the derby database
  *
  * @author Michael Erichsen
  * @version 2. mar. 2023
@@ -22,10 +24,19 @@ import java.util.logging.Logger;
  */
 public class SearchArchives {
 	private static final String KRONBORG = "Kronborg";
-	private static final String SELECT = "SELECT * FROM GEDCOM.EVENT "
+	private static final String SELECT_PROBATE = "SELECT * FROM GEDCOM.EVENT "
 			+ "JOIN GEDCOM.INDIVIDUAL ON GEDCOM.EVENT.ID = GEDCOM.INDIVIDUAL.EVENT_ID "
-			+ "WHERE GEDCOM.INDIVIDUAL.FONKOD = '%s'";
+			+ "WHERE GEDCOM.INDIVIDUAL.FONKOD = '%s' AND GEDCOM.EVENT.FROMDATE >= '%s' AND TODATE <= '%s'";
+	private static final String CENSUS_HEADER = "FTaar;KIPnr;Loebenr;Kildestednavn;Amt;Herred;Sogn;"
+			+ "Husstands_familienr;Matr_nr_Adresse;Kildenavn;Fonnavn;Koen;Alder;Civilstand;"
+			+ "Kildeerhverv;Stilling_i_husstanden;Kildefoedested;Foedt_kildedato;Foedeaar;"
+			+ "Adresse;Matrikel;Gade_nr;Kildehenvisning;Kildekommentar\n";
+	private static final String SELECT_CENSUS = "SELECT * FROM VEJBY.CENSUS WHERE FONNAVN = '%s' "
+			+ "AND FTAAR >= %s AND FTAAR <= %s";
+	private static final String PROBATE_HEADER = "GEDCOM NAME;ID;FROMDATE;TODATE;PLACE;EVENTTYPE;VITALTYPE;COVERED_DATA;SOURCE;FONKOD";
 	private static Logger logger;
+	private static int counter = 0;
+	private static BufferedWriter bw;
 
 	/**
 	 * Main method
@@ -33,9 +44,9 @@ public class SearchArchives {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if (args.length < 5) {
+		if ((args.length < 4) || (args.length > 6)) {
 			System.out.println(
-					"Usage: SearchArchives name vejbydatabasepath probatedatabasepath dddcsvfilepath outputdirectory");
+					"Usage: SearchArchives censusdatabasepath probatedatabasepath outputdirectory name [birthyear [deathyear]]");
 			System.exit(4);
 		}
 
@@ -72,17 +83,43 @@ public class SearchArchives {
 	 * @throws Exception
 	 */
 	private void execute(String[] args) throws Exception {
-
-		// Search DDD Census files
-//		searchDDDFiles(args);
-
 		// Search Census Derby table
 		searchCensusTable(args);
 
 		// Search for probates
-		findProbate(args);
+		searchProbates(args);
 
 		logger.info("Program ended");
+	}
+
+	/**
+	 * Search the census table for the individual
+	 *
+	 * @param args
+	 * @throws Exception
+	 */
+	private void searchCensusTable(String[] args) throws Exception {
+		final Statement statement = connectToDB(args[0]);
+
+		final String phonName = new Fonkod().generateKey(args[3]);
+		final String birthYear = (args.length < 5 ? "0" : args[4]);
+		final String deathYear = (args.length < 6 ? "9999" : args[5]);
+		final String query = String.format(SELECT_CENSUS, phonName, birthYear, deathYear);
+		logger.info(query);
+		final ResultSet rs = statement.executeQuery(query);
+
+		List<CensusIndividual> cil = null;
+
+		while (rs.next()) {
+			cil = CensusIndividual.getFromDb(rs);
+		}
+
+		statement.close();
+
+		if ((cil != null) && (cil.size() > 0)) {
+			writeCensusOutput(cil, args);
+		}
+
 	}
 
 	/**
@@ -92,11 +129,14 @@ public class SearchArchives {
 	 * @param args
 	 * @throws Exception
 	 */
-	private void findProbate(String[] args) throws Exception {
-		final Statement statement = connectToDB(args[2]);
+	private void searchProbates(String[] args) throws Exception {
+		final Statement statement = connectToDB(args[1]);
 
-		final String phonName = new Fonkod().generateKey(args[0]);
-		final String query = String.format(SELECT, phonName);
+		final String phonName = new Fonkod().generateKey(args[3]);
+		final String birthYear = (args.length < 5 ? "0001" : args[4]);
+		final String deathYear = (args.length < 6 ? "9999" : args[5]);
+		final String query = String.format(SELECT_PROBATE, phonName, birthYear + "-01-01", deathYear + "-12-31");
+		logger.info(query);
 		final ResultSet rs = statement.executeQuery(query);
 
 		String singleLine;
@@ -126,11 +166,9 @@ public class SearchArchives {
 		statement.close();
 
 		if (counter > 0) {
-			final String outName = args[4] + "/" + args[0] + "_probates.csv";
-			final BufferedWriter bw = new BufferedWriter(new FileWriter(outName));
-
-			final String header = "GEDCOM NAME;ID;FROMDATE;TODATE;PLACE;EVENTTYPE;VITALTYPE;COVERED_DATA;SOURCE;FONKOD";
-			bw.write(header + "\n");
+			final String outName = args[2] + "/" + args[3] + "_probates.csv";
+			bw = new BufferedWriter(new FileWriter(outName));
+			bw.write(PROBATE_HEADER + "\n");
 
 			for (final String string : outLines) {
 				bw.write(string + "\n");
@@ -146,35 +184,32 @@ public class SearchArchives {
 	}
 
 	/**
+	 * Write output for the census search
+	 * 
+	 * @param cil
 	 * @param args
+	 * @throws IOException
 	 */
-	private void searchCensusTable(String[] args) {
-		final String[] censusDbArgs = new String[3];
-		censusDbArgs[0] = args[0];
-		censusDbArgs[1] = args[1];
-		censusDbArgs[2] = args[4];
+	private void writeCensusOutput(List<CensusIndividual> cil, String[] args) throws IOException {
+		final String outName = args[2] + "/" + args[3] + "db_census.csv";
 
-		try {
-			new CensusFinderDbByName(censusDbArgs);
-		} catch (final Exception e) {
-			e.printStackTrace();
+		for (final CensusIndividual ci : cil) {
+			if (counter == 0) {
+				bw = new BufferedWriter(new FileWriter(new File(outName)));
+				bw.write(CENSUS_HEADER);
+			}
+
+			bw.write(ci.toString());
+			counter++;
+		}
+
+		if (counter > 0) {
+			bw.flush();
+			bw.close();
+			logger.info(counter + " lines of census data written to " + outName);
+
+			Desktop.getDesktop().open(new File(outName));
 		}
 	}
 
-	/**
-	 * @param args
-	 */
-//	private void searchDDDFiles(String[] args) {
-//		final String[] censusArgs = new String[4];
-//		censusArgs[0] = args[0];
-//		censusArgs[1] = args[1];
-//		censusArgs[2] = args[3];
-//		censusArgs[3] = args[4];
-//
-//		try {
-//			new CensusFinder(censusArgs);
-//		} catch (final Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
 }
